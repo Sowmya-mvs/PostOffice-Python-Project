95061e7880b4f231ffe5dbc49bc390ab9f2e1cb5
Sowmyashree M V <46842742+Sowmya-mvs@users.noreply.github.com>
2025-12-16 11:33:31 +0530

    Delete # Legacy /postoffice/postoffice directory

--- # Legacy /postoffice/postoffice/legacy_mail.py
+++
@@ -1,8 +0,0 @@
-import imp  # Deprecated since Python 3.4, removed in 3.12+
-
-def load_mail_module(path):
-    return imp.load_source('mail_module', path)
-
-def send_mail(address, message):
-    print "Sending mail to %s" % address   # Python 2 syntax
-    print message
