95e38e2987d14e832c7a927184f9e1c3d036c322
Sowmyashree M V <46842742+Sowmya-mvs@users.noreply.github.com>
2025-12-16 11:35:11 +0530

    Add MailBox class implementing MutableMapping

---
+++ postoffice/old_utils.py
@@ -0,0 +1,20 @@
+from collections import MutableMapping  # Deprecated import
+
+class MailBox(MutableMapping):
+    def __init__(self):
+        self.store = {}
+
+    def __getitem__(self, key):
+        return self.store[key]
+
+    def __setitem__(self, key, value):
+        self.store[key] = value
+
+    def __delitem__(self, key):
+        del self.store[key]
+
+    def __iter__(self):
+        return iter(self.store)
+
+    def __len__(self):
+        return len(self.store)
